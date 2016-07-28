f58a511247908f3b6d6e9ec2d524318179f8ac7e
ctcpip <ctcpip@users.noreply.github.com>
2016-07-28 00:36:49 -0500

    add 'help' legend

--- screen.go
+++ screen.go
@@ -36,19 +36,33 @@ func (s *screen) init() {
 
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 	s.drawText(strHeader, 0, 0)
+	drawHelp()
 	termbox.Flush()
 
 }
 
-func (s *screen) drawText(text string, startX, y int) {
+func drawHelp() {
+
+	scr.drawColoredText(" ^C ", 0, 23, termbox.ColorBlack, termbox.ColorWhite)
+	scr.drawText("Exit", 5, 23)
+	scr.drawColoredText(" P ", 13, 23, termbox.ColorBlack, termbox.ColorWhite)
+	scr.drawText("Pause Timer", 17, 23)
+
+}
+
+func (s *screen) drawColoredText(text string, startX, y int, fg, bg termbox.Attribute) {
 
 	currX := startX
 
 	for i := 0; i < len(text); i++ {
-		termbox.SetCell(currX, y, rune(text[i]), termbox.ColorDefault, termbox.ColorDefault)
+		termbox.SetCell(currX, y, rune(text[i]), fg, bg)
 		currX++
 	}
 
+}
+
+func (s *screen) drawText(text string, startX, y int) {
+	scr.drawColoredText(text, startX, y, termbox.ColorDefault, termbox.ColorDefault)
 }
 
 func (s *screen) close() {
